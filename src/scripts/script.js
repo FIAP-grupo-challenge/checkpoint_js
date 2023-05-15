81b6534ef04e2b2c62aae416df344d0cc2fa2b37
lucasfjoaquim <luacasfjoaquim@gmail.com>
2023-05-15 20:51:56 -0300

    valida se a nota esta entre 0 e 10

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,9 +1,13 @@
 document.getElementById("numbers-form").addEventListener("submit", function(event) {
     event.preventDefault();
+    
     var num1 = parseInt(document.getElementById("num1").value);
     var num2 = parseInt(document.getElementById("num2").value);
     var num3 = parseInt(document.getElementById("num3").value);
     var num4 = parseInt(document.getElementById("num4").value);
+    if ((num1 > 10 || num1 < 0)||(num2 > 10 || num2 < 0)||(num3 > 10 || num3 < 0)||(num4 > 10 || num4 < 0)) {
+        alert("voce digitou uma nota invalida")
+    } else {
     var media = (num1 + num2 + num3 + num4) / 4;
     if(media >= 7){
         alert("Aprovado com media : " + media);
@@ -11,5 +15,5 @@ document.getElementById("numbers-form").addEventListener("submit", function(even
         alert("Ficou de exame com media: " + media)
     } else {
         alert("Reprovou com media: " + media)
-    }
+    }}
    });
